48ced402d9392d538700d6d77c40ad6eea030183
沐琪 <106466776+zhxxkeke@users.noreply.github.com>
2023-12-07 14:07:12 +0800

    抖店-订单备注.js

--- 抖店-订单备注.js
+++ 抖店-订单备注.js
@@ -5,7 +5,7 @@
 // @description  none
 // @author       沐琪
 // @match        https://fxg.jinritemai.com/ffa/morder/order/list*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=jinritemai.com
+// @icon         https://lf1-fe.ecombdstatic.com/obj/eden-cn/upqphj/homepage/icon.svg
 // @grant        none
 // ==/UserScript==
 
@@ -13,42 +13,51 @@
     'use strict';
 
     // 当前日期
-    function getNowFormatDate() {
+    function _today() {
         const today = new Date();
         const month = String(today.getMonth() + 1);
         const day = String(today.getDate());
         return ` ${month}.${day} ZHUAN `
     }
 
-    // 编辑文本框
-    function editTextByReact() {
-        setInterval(function () {
-            const element = document.querySelector('#seller_words');
-            if (element) {
-                const inputValue = element.value + getNowFormatDate();
-                const valueSetter = Object.getOwnPropertyDescriptor(element, 'value').set;
-                const prototype = Object.getPrototypeOf(element);
-                const prototypeValueSetter = Object.getOwnPropertyDescriptor(prototype, 'value').set;
-                element.onclick = function () {
-                    if (element.value.indexOf('ZHUAN') === -1 && element.value.indexOf('转') === -1) {
-                        if (valueSetter && valueSetter !== prototypeValueSetter) {
-                            prototypeValueSetter.call(element, inputValue)
-                        } else {
-                            valueSetter.call(element, inputValue)
-                        }
-                        setTimeout(function () {
-                            // 保存
-                            document.querySelector('[data-btm="d440734"] > span').click()
-                        }, 500)
-                    }
-                }
-                element.dispatchEvent(new Event('input', {bubbles: true}))
-            }
-        }, 100)
+    // 异步延时
+    function delay(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
     }
 
-    // 运行
-    window.onload = function () {
-        editTextByReact()
+
+    function main() {
+        const page = document.querySelector('#__GARFISH_SSR_ECOM_FXG_ADMIN_DOM');
+        const search = page.querySelector('button[type="submit"]');
+        search.addEventListener('click', function () {
+            let timerId;
+            timerId = setInterval(() => {
+                const input = document.querySelector('#seller_words');
+                if (input) {
+                    const inputValue = input.value + _today();
+                    const valueSetter = Object.getOwnPropertyDescriptor(input, 'value').set;
+                    const prototype = Object.getPrototypeOf(input);
+                    const prototypeValueSetter = Object.getOwnPropertyDescriptor(prototype, 'value').set;
+                    input.onclick = async function () {
+                        if (input.value.indexOf('ZHUAN') === -1 && input.value.indexOf('转') === -1) {
+                            if (valueSetter && valueSetter !== prototypeValueSetter) {
+                                prototypeValueSetter.call(input, inputValue)
+                            } else {
+                                valueSetter.call(input, inputValue)
+                            }
+                            await delay(200);
+                            document.querySelector('[data-btm="d440734"] > span').click()
+                        }
+                        clearInterval(timerId)
+                    }
+                    input.dispatchEvent(new Event('input', {bubbles: true}))
+                }
+            }, 100);
+        });
     }
+
+
+    window.addEventListener('load', function () {
+        main();
+    })
 })();
